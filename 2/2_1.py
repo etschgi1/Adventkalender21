9443451cab3220b15fe4e6511e848e185feb9fa3
Elias Wachmann <e.wachmann@student.tugraz.at>
2021-12-02 16:17:02 +0100

    2/24

---
+++ 2/2_1.py
@@ -0,0 +1,33 @@
+FILENAME = "./input.txt"
+FILENAME2 = "./input_test.txt"
+horizontal_pos = 0
+depth = 0
+
+
+def main():
+    f = open(FILENAME, "r")
+    l = f.readlines()
+    l = [x.strip("\n") for x in l]
+    dir_, dist = [], []
+    for e in l:
+        c = e.split(" ")
+        dir_.append(c[0])
+        dist.append(c[1])
+    distance = {"forward": 0, "up": 0, "down": 0}
+    aim = 0
+    depth = 0
+    for c, e in enumerate(dir_):
+        if e == "down":
+            aim += int(dist[c])
+        elif e == "up":
+            aim -= int(dist[c])
+        else:  # forward
+            distance["forward"] += int(dist[c])
+            depth += aim*int(dist[c])
+    # print(distance, distance["down"]-distance["up"],
+    #       (distance["down"]-distance["up"])*distance["forward"])
+    print(distance["forward"], depth, depth*distance["forward"])
+
+
+if __name__ == "__main__":
+    main()
